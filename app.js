ac3e528eacfd69b1916d098b103b963017bd387e
Kenny Jinhiro <83710171+jxnhiro@users.noreply.github.com>
2023-08-24 17:29:48 +0700

    1st Chapter - Status Codes and HTTP Server

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-console.log('Express Tutorial')
+// HTTP comes in built-in module from node.
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {'content-type' : 'text/html'});
+    res.write('<h1>Home Page</h1>');
+    res.end();
+});
+
+// React 3000, Gatsby 8000
+server.listen(5000);
+
+/**
+ * HTTP Status Codes
+ * ================================= 
+ * 
+ * Informational responses (100-199)
+ * Successful responses (200 - 299)
+ * Redirects (300-399)
+ * Client errors (400-499)
+ * Server errors (500-599)
+ * 
+ * Go to developer.mozilla.org/en-US/docs/Web/HTTP/Status to learn more about status codes.
+ */
