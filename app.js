fcf56f2d73d89e246c653aa785e08db6fcf13a33
Kenny Jinhiro <83710171+jxnhiro@users.noreply.github.com>
2023-08-24 17:40:36 +0700

    2nd Chapter - Request Object

--- app.js
+++ app.js
@@ -2,6 +2,15 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
+
+    /** Request object has two important properties.
+    * First is url, to know the URL from the request after the IP, Example: 192.168.1.1/about, then /about is sent.
+    * Second is method, to know the method that is wanted by the browser. In browser's case it's almost always GET.
+    **/
+   
+    console.log('URL in Request', req.url);
+    
+    // You attach the status code to know what is happening to the browser.
     res.writeHead(200, {'content-type' : 'text/html'});
     res.write('<h1>Home Page</h1>');
     res.end();
