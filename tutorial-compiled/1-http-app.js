33a40b281b24aabeff7cb27244091f2dba807b2a
Kenny Jinhiro <83710171+jxnhiro@users.noreply.github.com>
2023-08-24 19:09:24 +0700

    6th Chapter - Added HTTP Status Codes in HTTP App

--- tutorial-compiled/1-http-app.js
+++ tutorial-compiled/1-http-app.js
@@ -3,7 +3,10 @@ const http = require('http');
 const { readFileSync } = require('fs');
 
 //Get All Files
-const homePage = readFileSync('./index.html');
+const homePage = readFileSync('./navbar-app/index.html');
+const homeStyles = readFileSync('./navbar-app/styles.css');
+const homeImage = readFileSync('./navbar-app/logo.svg');
+const homeLogic = readFileSync('./navbar-app/browser-app.js');
 
 const server = http.createServer((req, res) => {
 
@@ -12,6 +15,20 @@ const server = http.createServer((req, res) => {
     * Second is method, to know the method that is wanted by the browser. In browser's case it's almost always GET.
     **/
     const url = req.url;
+
+    /**
+     * HTTP Status Codes
+     * ================================= 
+     * 
+     * Informational responses (100-199)
+     * Successful responses (200 - 299)
+     * Redirects (300-399)
+     * Client errors (400-499)
+     * Server errors (500-599)
+     * 
+     * Go to developer.mozilla.org/en-US/docs/Web/HTTP/Status to learn more about status codes.
+    **/
+
     if (url === '/') {
         //Status Message right here is optional.
         res.writeHead(200, {'content-type' : 'text/html'});
@@ -20,6 +37,18 @@ const server = http.createServer((req, res) => {
     } else if (url === '/about') {
         res.writeHead(200, {'content-type': 'text/html'});
         res.write('<h1>About Page</h1>');
+        res.end();
+    } else if (url === '/styles.css') {
+        res.writeHead(200, {'content-type' : 'text/css'});
+        res.write(homeStyles);
+        res.end();
+    } else if (url === '/logo.svg') {
+        res.writeHead(200, {'content-type' : 'image/svg+xml'});
+        res.write(homeImage);
+        res.end();
+    } else if (url === '/browser-app.js') {
+        res.writeHead(200, {'content-type' : 'text/javascript'});
+        res.write(homeLogic);
         res.end();
     } else {
         res.writeHead(400, {'content-type' : 'text/html'});
@@ -35,15 +64,3 @@ const server = http.createServer((req, res) => {
 // React 3000, Gatsby 8000
 server.listen(5000);
 
-/**
- * HTTP Status Codes
- * ================================= 
- * 
- * Informational responses (100-199)
- * Successful responses (200 - 299)
- * Redirects (300-399)
- * Client errors (400-499)
- * Server errors (500-599)
- * 
- * Go to developer.mozilla.org/en-US/docs/Web/HTTP/Status to learn more about status codes.
- */
