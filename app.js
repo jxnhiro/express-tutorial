408d960997bc24fe386d70a117a453dade8d18dc
Kenny Jinhiro <83710171+jxnhiro@users.noreply.github.com>
2023-08-24 17:44:29 +0700

    3rd Chapter - Further Request Object and Funnelling of Request URL

--- app.js
+++ app.js
@@ -7,13 +7,25 @@ const server = http.createServer((req, res) => {
     * First is url, to know the URL from the request after the IP, Example: 192.168.1.1/about, then /about is sent.
     * Second is method, to know the method that is wanted by the browser. In browser's case it's almost always GET.
     **/
-   
+    const url = req.url;
+    if (url === '/') {
+        //Status Message right here is optional.
+        res.writeHead(200, {'content-type' : 'text/html'});
+        res.write('<h1>Home Page</h1>');
+        res.end();
+    } else if (url === '/about') {
+        res.writeHead(200, {'content-type': 'text/html'});
+        res.write('<h1>About Page</h1>');
+        res.end();
+    } else {
+        res.writeHead(400, {'content-type' : 'text/html'});
+        res.write('<h1>Page not found.</h1>');
+        res.end();
+    }
     console.log('URL in Request', req.url);
     
     // You attach the status code to know what is happening to the browser.
-    res.writeHead(200, {'content-type' : 'text/html'});
-    res.write('<h1>Home Page</h1>');
-    res.end();
+    
 });
 
 // React 3000, Gatsby 8000
